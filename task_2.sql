9dfd8649d5aea33ebbb71ac2282deb18bc227f99
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-06-29 22:26:58 +0100

    Fix: Rename author column to author_name for ALX checker

--- task_2.sql
+++ task_2.sql
@@ -2,7 +2,7 @@ USE alx_book_store;
 
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    author_name VARCHAR(100) NOT NULL,
     bio TEXT
 );
 
